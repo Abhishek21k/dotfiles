aa50ec04736297f7971cbd30cb39d7a593e5ab05
Abhishek Kumar <abhishek21k15@gmail.com>
2026-02-24 10:18:05 +0530

    added a frecency for the file picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,9 @@ return {
     animate = { enabled = false },
     scroll = { enabled = false },
     picker = {
+      matcher = {
+        frecency = true,
+      },
       sources = {
         files = {
           hidden = true,
@@ -32,6 +35,22 @@ return {
         grep = {
           hidden = false,
           ignored = false,
+          exclude = {
+            ".git", ".DS_Store",
+            -- JS/TS
+            "node_modules", "dist", ".next", "build",
+            "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb",
+            -- React Native / Expo
+            ".expo", "android", "ios",
+            -- Rust
+            "target", "Cargo.lock",
+            -- Python
+            "venv", ".venv", "env", "__pycache__", ".pytest_cache", ".tox", ".mypy_cache", ".ruff_cache",
+            -- Go
+            "vendor", "go.sum",
+            -- Dart / Flutter
+            ".dart_tool", ".pub-cache", ".pub",
+          },
         },
         explorer = {
           hidden = true,
